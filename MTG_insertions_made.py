f997f9e7e7c7312fef2c9f3bd537f5524540ea77
Austintensity <83841085+Austintensity@users.noreply.github.com>
2021-05-07 17:20:32 -0400

    Add files via upload

---
+++ MTG_insertions_made.py
@@ -0,0 +1,47 @@
+text = """Artifact, instant, and sorcery spells your opponents cast cost {1} more to cast. - : Until your next turn, prevent all damage that would be dealt to and dealt by target permanent an opponent controls."""
+	
+print (len(text))
+	# game_notes = "Brudiclad played well, but Chulane won in the end"
+	# insert_game(1, ['Chulane', 'Talrand', 'Magda', 'Brudiclad'], 'Chulane', 'Talrand', 4, game_notes)
+		
+	# insert_deck('U-Dcsok02', 'Talrand Counterspell', 'U', 'Talrand', 1, 'PRIMARY', 0 , 0, 0, 0)
+	# insert_deck('UR-D01', "It is a Niv Mizzet visit, isn't it?  It is!", 'UR', 'Niv Mizzet Parun', 2, 'PRIMARY', 0 , 0, 0, 0)
+	# insert_deck('BRG-L01', "Lord Windgrace's destructive flow", 'BRG', 'Lord Windgrace', 3, 'PRIMARY', 0 , 0, 0, 0)
+	# insert_deck('BRG-STok03', "I wanna be Korvold, sucka!", 'BRG', 'Korvold', 3, 'SECONDARY', 0 , 0, 0, 0)
+	# insert_deck('WUG-Lc02', "I Chu-Chu-Chulane you", 'WUG', 'Chulane, Teller of Tales', 3, 'PRIMARY', 0 , 0, 0, 0)
+	# insert_deck('WUBRG-T01', "Smithers, Who is this Gastropod?", 'WUBRG', 'Sliver Overloard', 5, 'PRIMARY', 0 , 0, 0, 0)
+
+	# insert_planeswalker('Animatou, the Fateshifter', 3, 'FALSE', 'TRUE', 'TRUE', 'TRUE')
+	# insert_planeswalker('Lord Windgrace', 5, 'FALSE', 'TRUE', 'TRUE', 'TRUE')
+	# insert_planeswalker('Saheeli, the Gifted', 3, 'FALSE', 'TRUE', 'TRUE', 'TRUE')	
+	# insert_planeswalker('The Wanderer', 5, 'TRUE', 'FALSE', 'TRUE', 'FALSE')	
+	# insert_planeswalker('Davriel, Rogue Shadowmage', 3, 'TRUE', 'FALSE', 'TRUE', 'FALSE')	
+
+	
+	# insert_art_ench('Opal Titan', 'Enchantment', 'FALSE', 'TRUE', 'FALSE', 'Enchantment that transforms into Creature')
+	# insert_art_ench('Maul of the Skyclaves', 'Artifact Equipment', 'TRUE', 'FALSE', 'FALSE', 'Equipment gives +2/+2, Flying and First Strike')
+	# insert_art_ench('Rally the Ranks', 'Enchantment', 'FALSE', 'FALSE', 'TRUE', 'Creatures you control of the <ETB> chosen type get +1/+1')
+	# insert_art_ench('Sol Ring', 'Artifact Mana Rock', 'TRUE', 'FALSE', 'FALSE', 'Tap to add 2 colourless mana')
+	# insert_art_ench('Arcane Signet', 'Artifact Mana Rock', 'TRUE', 'FALSE', 'FALSE', "Tap to add 1 mana of any colour in your commander's identity")
+	# insert_art_ench('Robe of Mirrors', 'Enchantment Aura', 'FALSE', 'FALSE', 'TRUE', 'Enchanted Creature has shroud')
+	# insert_art_ench('Overwhelmeing Instinct', 'Enchantment', 'FALSE', 'TRUE', 'FALSE', 'When you attack with 3 or more creatures, draw a card')
+	# insert_art_ench('Dark Prophecy', 'Enchantment', 'FALSE', 'TRUE', 'FALSE', 'When a creature you control dies, draw a card and lose 1 life')
+	# insert_art_ench("Cultivator's Caravan", 'Artifact Vehicle', 'TRUE', 'FALSE', 'FALSE', "Tap to add 1 mana of any colour")
+
+	# insert_land('Command Tower', 'WUBRG', 1, 0)
+	# insert_land('Temple of the False God', 'C', 2, 0)
+	# insert_land('Boros Garrison', 'WR', 2, 0)
+	# insert_land('Rejuvinating Springs', 'UG', 1, 0)
+	# insert_land('Barren Moor', 'B', 1, 1)
+	# insert_land('Plains', 'W', 1, 0)
+	# insert_land('Swamp', 'B', 1, 0)
+	# insert_land('Mountain', 'R', 1, 0)
+	# insert_land('Forest', 'G', 1, 0)
+	# insert_land('Island', 'U', 1, 0)
+	
+	# insert_creature('Chulane, Teller of Tales','Human',2,4)
+	# insert_creature('Zada, Hedron Collider','Goblin',3,3)
+	# insert_creature('Priest of Urabrask','Human Cleric',2,1)
+	# insert_creature('Hellkite Dragon','Dragon',5,5)
+	# insert_creature('Dockside Extortionist','Goblin',1,1)
+	# insert_creature('Talrand','Merfolk Wizard',2,2)
